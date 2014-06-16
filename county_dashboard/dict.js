818ca25db49f9b27ad8e0c31ace021a660fe7725
Gregor Passolt <gregor.passolt@gmail.com>
2014-06-16 16:46:41 -0700

    Dashboard when Gloria left

---
+++ county_dashboard/dict.js
@@ -0,0 +1,17 @@
+      var small_counties = ["Adams","Columbia", "Ferry", "Garfield", "Lincoln", "Pend_Oreille", "San Juan", "Skamania", "Wahkiakum"];
+      
+      var fast_fact_desc = [
+          {"id":"trend_0","title":"Entries per 1,000","descrption":"The minimum and maximum date for which reliable data are available are indicated on the slide bar. You can adjust the bar to any range; the default is the full date range."},
+          {"id":"trend_1","title":"Entries Age 0-4","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"trend_2","title":"Entries with Immediate Dependency","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"trend_3","title":"Placements in Kin Care" ,"descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"trend_4","title":"Siblings All or Some Together","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+      ]; 
+                   
+      var population_desc = [
+          {"id":"population_0","title":"Population < 18","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"population_1","title":"% Children < 5","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"population_2","title":"Unemployment","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"population_3","title":"H.S. Grad Rate","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"},
+          {"id":"population_4","title":"Free/Reduced Lunch","descrption":"bla bla bla bla bla bla bla bla bla bla bla bl"}
+      ];
